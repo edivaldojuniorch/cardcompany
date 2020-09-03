364a99f255e0c54ed334b38fb2ab8f0b7a84fa22
edivaldojuniorch <joseedivaldocg@hotmail.com>
2020-09-03 00:30:53 -0300

    initial commit

---
+++ src/Componentes/Navbar.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+const Navbar = () => {
+  return (
+    <>
+      <div className="navbar">
+        <div className="navbar-container container">
+          <Link className="navbar-logo">
+            Quarto Quadrante
+          </Link>
+          <div className="menu-icon">
+            {}
+          </div>
+        </div>
+
+
+
+
+      </div>
+    </>
+  );
+}
+
+export default Navbar;
